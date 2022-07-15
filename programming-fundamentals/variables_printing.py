0544cc47d1e40a491b50b210d1fadbc4e9726e4e
subrato29 <subratosarkar29@gmai.com>
2022-07-15 15:07:41 -0400

    variable printing concept added

---
+++ programming-fundamentals/variables_printing.py
@@ -0,0 +1,16 @@
+a = 1
+b = "hello"
+c = True
+
+print(a, b, c)
+
+print ("hello", 10, True)
+print ("hello" + str(10))
+
+print ('Hello', end = ' | ')
+print ('World')
+
+"""
+nameTwo => variable name of this time not preferred in Python
+name_two => preferred
+"""
