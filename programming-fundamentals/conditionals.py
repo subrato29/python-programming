1b98b412db396037dfdffab45478b6aa4671506f
subrato29 <subratosarkar29@gmai.com>
2022-07-16 16:44:33 -0400

    conditional if else added

---
+++ programming-fundamentals/conditionals.py
@@ -0,0 +1,27 @@
+# Welcome to our Python playground!
+
+a = 1
+b = 2
+
+if a == 1:
+    print("a is equal to 1")
+
+if b == 1:
+    print("b is equal to 1")
+elif b == 2:
+    print("b is equal to 2")
+else:
+    print("b is not equal to 1 and not equal to 2")
+
+x = 6
+
+result = "OK" if x > 5 else "NOT OK"
+print (result)
+
+
+if x > 5:
+    print ("SUCCESS")
+    print ("TRUST ME")
+else:
+    print ("FAILURE")
+    print ("DONT TRUST ME")
