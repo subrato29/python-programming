c3dbd3a75af334adfa3465fa368cff8f04868c3f
subrato29 <subratosarkar29@gmai.com>
2022-07-16 13:23:37 -0400

    type conversion concept added

---
+++ programming-fundamentals/type_conversions.py
@@ -0,0 +1,15 @@
+# Welcome to our Python playground!
+
+a = "42"
+b = int(a)
+
+print (int(a) + 50)
+print (float(a) + 50)
+
+print (int(20.4) + 80)
+print (bool("8"))
+print (bool(0))
+print (bool(""))
+
+print(a, type(a))
+print(b, type(b))
