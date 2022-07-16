0cafcfda8243569251ecce96ae7a924365f2bdf3
subrato29 <subratosarkar29@gmai.com>
2022-07-16 16:20:29 -0400

    adding conditional operator

---
+++ programming-fundamentals/conditions.py
@@ -0,0 +1,29 @@
+# Welcome to our Python playground!
+
+a = 1
+b = 2
+c = 3
+
+print(a == 1)
+print(a == 2)
+print(a + b == c)
+
+print(a - b > c)
+print(a ** b < c)
+
+cond = 3.0 == 3
+print (cond)
+
+cond = 3.1 != 3
+print (cond)
+print (6 != '6')
+
+str1 = "ac"
+str2 = "ab"
+print (str1 <= str2)
+
+print (ord("a"))
+print (ord("A"))
+print ("a" > "A")
+print (chr(76))
+print (chr(65))
