955c34eee0c95d66ba5a2bfc7dc7da3b13db9a2a
subrato29 <subratosarkar29@gmai.com>
2022-07-17 18:11:30 -0400

    adding slice feature

---
+++ programming-fundamentals/slices.py
@@ -0,0 +1,17 @@
+#List
+list = [1, 2, 3, 4, 5, 6]
+print (list[1:4])
+print (list[:4])
+print (list[:len(list):2])
+print (list[:])
+print (list[::2])
+print (list[::-1]) #reverse
+
+#String
+string = "hello world"
+print (string[::2])
+print (string[1:6:2])
+
+#Tuple
+tuple = (1, 2, 3, 4, 5, 6)
+print (tuple[1:6:2])
