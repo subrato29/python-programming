5c6277eadbc52e9cc29e7bfd9596dac8d3ade512
subrato29 <subratosarkar29@gmai.com>
2022-07-19 12:42:35 -0400

    misc syntax added

---
+++ programming-fundamentals/misc_syntax.py
@@ -0,0 +1,20 @@
+lst = [i for i in range(1, 11)]
+print(lst)
+
+x, y = 1, 2
+print(x, y)
+
+x, y, z = (1, 2, 3)  # Unpacking
+print(x, y, z)
+
+
+# doc string
+def foo():
+    """
+    This is a foo function
+    """
+    pass
+
+
+help(foo)  # tells what the function does
+# help (len)
