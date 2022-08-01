f3e516584d6d76b3fb449caeee92054a87990bcb
subrato29 <subratosarkar29@gmai.com>
2022-08-01 10:25:12 -0400

    random guess problem added

---
+++ programming-fundamentals/assessment/random_number_guesser.py
@@ -0,0 +1,33 @@
+import random
+
+# Write your code here.
+# Make sure to use `random.randint` to pick your random number.
+start = input("Enter the start of the range: ")
+while not start.isdigit():
+    print("Enter a valid number: ")
+    start = input("Enter the start of the range: ")
+
+end = input("Enter the end of the range: ")
+while not end.isdigit() or int(end) < int(start):
+    print("Enter a valid number: ")
+    end = input("Enter the end of the range: ")
+
+random_number = random.randint(int(start), int(end))
+
+guess = None
+attempt = 0
+
+while guess != random_number:
+    guessed_number = input("Continue guessing: ")
+    if not guessed_number.isdigit():
+        print("Your input is not a valid number.")
+        continue
+
+    attempt += 1
+    guess = int(guessed_number)
+
+suffix = ""
+if attempt > 1:
+    suffix = "s"
+
+print("You guess the right number, which is:", guess, " in", attempt, "attempt" + suffix)
