0a4e385beddc978a30ee5adb80a884a6f67894ed
subrato29 <subratosarkar29@gmai.com>
2022-08-14 12:37:25 -0400

    formatted

--- programming-fundamentals/type_conversions.py
+++ programming-fundamentals/type_conversions.py
@@ -3,13 +3,13 @@
 a = "42"
 b = int(a)
 
-print (int(a) + 50)
-print (float(a) + 50)
+print(int(a) + 50)
+print(float(a) + 50)
 
-print (int(20.4) + 80)
-print (bool("8"))
-print (bool(0))
-print (bool(""))
+print(int(20.4) + 80)
+print(bool("8"))
+print(bool(0))
+print(bool(""))
 
 print(a, type(a))
 print(b, type(b))
